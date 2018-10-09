c2196a4f51399409cf96a81684424e0e820a1806
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-09 13:05:56 +0200

    Function addItems done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
- const items = [];
+const items = [];
+
+
+function addItem (e) {
+    e.preventDefault(); // prevent page reload
+    const text = (this.querySelector('[name=item]')).value;
+    const item = {
+        text,
+        done: false
+    }
+    items.push(item);
+    this.reset();
+
+}
+
+
+
+
+// we're listening for submit event of form, not click event on button, because we want to be sure, that when someone will click enter it will also be noticed
+addItems.addEventListener('submit', addItem);
