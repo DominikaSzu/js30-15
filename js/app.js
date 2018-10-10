fabb31e521faad8e4b256a312b5aa4204ef79521
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-10 10:31:20 +0200

    Tapas added to localStorage

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 
 function addItem (e) {
     e.preventDefault(); // prevent page reload
     const text = (this.querySelector('[name=item]')).value;
     const item = {
-        text
-//        text,
-//        done: false
+        text,
+        done: false
     }
     items.push(item);
     populateList(items, itemsList);
@@ -36,3 +35,5 @@ function populateList(plates =[], platesList) {
 
 // we're listening for submit event of form, not click event on button, because we want to be sure, that when someone will click enter it will also be noticed
 addItems.addEventListener('submit', addItem);
+
+populateList(items, itemsList);
